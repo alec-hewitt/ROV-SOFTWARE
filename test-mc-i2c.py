faf0245c8fb79032e225512d9ce01062cc8717d7
Alec Hewitt <ahewitt0000@gmail.com>
2024-02-16 23:26:09 -0500

    init

--- test-mc-i2c.py
+++ test-mc-i2c.py
@@ -1,10 +1,10 @@
 import smbus
 
+class I2CBUS(smbus.SMBus):
 
-class i2c_bus(self, bus_number:int):
-
-    self.bus_num = bus_number
-    self.bus = None
+    def __init__(self, bus_number: int):
+        self.bus_num = bus_number
+        self.bus = None
 
     def __init__(self):
         self.open_bus(self.bus_num)
@@ -13,16 +13,17 @@ class i2c_bus(self, bus_number:int):
         bus = smbus.SMBus(self.bus_num)
 
     def close_bus(self):
-        if self.bus != None:
+        if self.bus is not None:
             self.bus.close()
         else:
             print("ERROR: You are trying to close a bus that has not been opened!")
 
-    def write_byte(self, addr:int, b:int):
-        if self.bus != None:
+    def write_byte(self, addr: int, b: int):
+        if self.bus is not None:
             self.bus.write_byte(addr, b)
         else:
             print("ERROR: You are trying to write to a bus that has not been opened!")
+
 
 bus = i2c_bus(1)
 
